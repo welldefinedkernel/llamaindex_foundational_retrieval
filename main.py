deee27b32cfdd67d1f7a84259e1a77cb5ac20bc7
Roman Tomchik <welldefinedkernel@gmail.com>
2026-04-13 13:02:13 +0200

    feat: setup future pipeline dev

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    ...
+from datasets import load_dataset
+from llama_index.core import VectorStoreIndex
+from deepeval.models.base_model import DeepEvalBaseLLM
 
+def main():
+    ragbench = load_dataset("rungalileo/ragbench", "techqa", split="train")
 
 if __name__ == "__main__":
     main()
