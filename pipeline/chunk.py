cc2efaea2194b2c52f6cded4bbf47ed66803077a
Roman Tomchik <welldefinedkernel@gmail.com>
2026-04-15 12:33:32 +0200

    feat(pipeline/chunk.py): start working on embeddings

--- pipeline/chunk.py
+++ pipeline/chunk.py
@@ -1,6 +1,13 @@
 from llama_index.core.node_parser import SentenceSplitter
+from sentence_transformers import SentenceTransformer
 
 def chunk_documents(documents, chunk_size=512, chunk_overlap=0):
     splitter = SentenceSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     nodes = splitter.get_nodes_from_documents(documents)
     return nodes
+
+def embed_chunks(chunks, embed_model):
+    model = SentenceTransformer(embed_model, model_kwargs={"dtype": "auto"})
+    for chunk in chunks:
+        chunk.embedding = model.encode(chunk.get_content(), prompt_name="embedding")
+    return chunks
