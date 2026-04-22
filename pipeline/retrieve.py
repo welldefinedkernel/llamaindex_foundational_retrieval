7c5445748af6ab8ed59957f3f3ccb2ef6004e1cc
Roman Tomchik <welldefinedkernel@gmail.com>
2026-04-22 14:09:01 +0200

    feat(pipeline/retrieve.py): implement query embedding and retrieval of top k docs

---
+++ pipeline/retrieve.py
@@ -0,0 +1,21 @@
+from llama_index.core.schema import NodeWithScore
+from llama_index.core.vector_stores.types import VectorStoreQuery, VectorStoreQueryResult
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from sentence_transformers import SentenceTransformer
+
+def embed_query(query: str, embed_model: str) -> list[float]:
+    model = SentenceTransformer(embed_model, model_kwargs={"dtype": "auto"})
+    return model.encode(query, prompt_name="web_search_query").tolist()
+    
+def retrieve_relevant_chunks(
+        query: str, 
+        vector_store: MilvusVectorStore,
+        embed_model: str,
+        top_k: int = 5, 
+    ) -> VectorStoreQueryResult:
+    query_embedding = embed_query(query, embed_model)
+    result = vector_store.query(
+        VectorStoreQuery(query_embedding=query_embedding, similarity_top_k=top_k)
+    )
+    
+    return result
