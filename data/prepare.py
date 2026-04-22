ec46ec429506bd147ed4a610e5a8bd6c7a50eb56
Roman Tomchik <welldefinedkernel@gmail.com>
2026-04-22 14:08:21 +0200

    fix(data/prepare.py): fix insertion of duplicate docs

--- data/prepare.py
+++ data/prepare.py
@@ -4,10 +4,12 @@ from typing import Any, cast
 
 def create_dataset_from_hf(dataset_name: str, subset_name: str, split: str):
     dataset = load_dataset(dataset_name, subset_name, split=split)
+    seen = set()
     documents = []
     for row in dataset:
         row_dict = cast(dict[str, Any], row)
         for doc_text in row_dict['documents']:
-            doc = Document(text=doc_text)
-            documents.append(doc)
+            if doc_text not in seen:
+                seen.add(doc_text)
+                documents.append(Document(text=doc_text))
     return documents
