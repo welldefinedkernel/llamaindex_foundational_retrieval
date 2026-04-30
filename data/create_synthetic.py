0b93db5fdc4c116540874188061349cf1289c004
Roman Tomchik <welldefinedkernel@gmail.com>
2026-04-30 11:39:55 +0200

    feat(data/create_synthetic.py): implement a creation of synthetic dataset over provided files using DeepEval

--- data/create_synthetic.py
+++ data/create_synthetic.py
@@ -1,10 +1,17 @@
 from dotenv import load_dotenv
 from deepeval.synthesizer import Synthesizer
+from deepeval.synthesizer.config import ContextConstructionConfig
 from pathlib import Path
 
 load_dotenv()
 
-def create_synthetic_dataset(docs_dir: str, output_dir: str, max_goldens_per_context: int = 2) -> None:
+def create_synthetic_dataset(
+        docs_dir: str, 
+        output_dir: str, 
+        max_contexts_per_document:int = 3,
+        max_goldens_per_context: int = 2
+    ) -> None:
+    
     docs_path = Path(docs_dir).resolve()
     output_path = Path(output_dir).resolve()
 
@@ -12,13 +19,22 @@ def create_synthetic_dataset(docs_dir: str, output_dir: str, max_goldens_per_con
     if not document_paths:
         raise FileNotFoundError(f"No .docx files found in {docs_dir}")
 
-    synthesizer = Synthesizer()
+    synthesizer = Synthesizer(cost_tracking=True)
+    context_config = ContextConstructionConfig(
+        max_contexts_per_document=max_contexts_per_document,
+        chunk_size=1024,
+        chunk_overlap=50
+    )
+
     synthesizer.generate_goldens_from_docs(
         document_paths=document_paths,
         max_goldens_per_context=max_goldens_per_context,
+        context_construction_config=context_config
     )
     synthesizer.save_as(
         file_type="json",
         directory=str(output_path),
         file_name="applets_4_0_synthetic",
     )
+
+    print(f"DeepEval synthesis cost: ${synthesizer.synthesis_cost:.6f}")
